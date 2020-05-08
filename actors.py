cce8a70117e7552b487d9d89a6f98395621d57a4
AleksanderUrbanowicz <57544016+AleksanderUrbanowicz@users.noreply.github.com>
2020-05-08 04:10:50 +0200

    cleanups

--- actors.py
+++ actors.py
@@ -11,18 +11,18 @@ SEPARATION_VALUES = {
 
 def log_unreal():
     unreal.log("hello Unreal")
-
-
-def separate_actors(label_prefix):
-    level_actors = unreal.EditorLevelLibrary.get_all_level_actors()
-    print('separate actors with prefix: '+label_prefix)
-    for actor in level_actors:
-        actor_label = actor.get_actor_label()
-        if label_prefix in actor_label:
-            print('placing: {}'.format(actor_label))
-            actor_location = actor.get_actor_location()
-            actor_bounds = actor.get_actor_bounds(False)
-            actor_min_z = actor_bounds[0].z - actor_bounds[1].z
-            location_offset = Vector(actor_location.x, actor_location.y, SEPARATION_VALUES['CUBE'])
-            actor.set_actor_location(location_offset, False, False)
+#
+#kod  z internetu
+# def separate_actors(label_prefix):
+#     level_actors = unreal.EditorLevelLibrary.get_all_level_actors()
+#     print('separate actors with prefix: '+label_prefix)
+#     for actor in level_actors:
+#         actor_label = actor.get_actor_label()
+#         if label_prefix in actor_label:
+#             print('placing: {}'.format(actor_label))
+#             actor_location = actor.get_actor_location()
+#             actor_bounds = actor.get_actor_bounds(False)
+#             actor_min_z = actor_bounds[0].z - actor_bounds[1].z
+#             location_offset = Vector(actor_location.x, actor_location.y, SEPARATION_VALUES['CUBE'])
+#             actor.set_actor_location(location_offset, False, False)
 
--- assets.py
+++ assets.py
@@ -1,14 +1,14 @@
 import unreal
 from unreal import Vector
-
-lst_actors = unreal.EditorLevelLibrary.get_all_level_actors()
-print('place actors at 0 z')
-for act in lst_actors:
-    act_label = act.get_actor_label()
-    if 'Cube' in act_label:
-        print('placing: {}'.format(act_label))
-        act_location = act.get_actor_location()
-        act_bounds = act.get_actor_bounds(False)
-        act_min_z = act_bounds[0].z - act_bounds[1].z
-        location_offset = Vector(act_location.x, act_location.y, act_location.z - act_min_z)
-        act.set_actor_location(location_offset, False, False)
+#kod z internatu
+# lst_actors = unreal.EditorLevelLibrary.get_all_level_actors()
+# print('place actors at 0 z')
+# for act in lst_actors:
+#     act_label = act.get_actor_label()
+#     if 'Cube' in act_label:
+#         print('placing: {}'.format(act_label))
+#         act_location = act.get_actor_location()
+#         act_bounds = act.get_actor_bounds(False)
+#         act_min_z = act_bounds[0].z - act_bounds[1].z
+#         location_offset = Vector(act_location.x, act_location.y, act_location.z - act_min_z)
+#         act.set_actor_location(location_offset, False, False)
